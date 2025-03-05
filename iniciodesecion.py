42240c6c4642bb722198c71eade4ddc68140dcb5
adrian dañon <adriancanon@unisangil.edu.co>
2025-03-05 16:17:18 -0500

    ejemplo de uso de operadores logicos

---
+++ iniciodesecion.py
@@ -0,0 +1,6 @@
+adrian ="dadrian"
+contraseña = "1234"
+usuario = input("Ingrese su usuario: ")
+contraseña = input("Ingrese su contraseña: ")
+print(usuario == adrian and contraseña == contraseña)
+
