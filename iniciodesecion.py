5d4f245bb5473cac98bd9a514ddf87fbf4aa8b17
adrian dañon <adriancanon@unisangil.edu.co>
2025-03-08 19:49:49 -0500

    sss

--- iniciodesecion.py
+++ iniciodesecion.py
@@ -1,6 +1,6 @@
 adrian ="dadrian"
-contraseña = "1234"
+contraseña = 1234
 usuario = input("Ingrese su usuario: ")
-contraseña = input("Ingrese su contraseña: ")
+contraseña = int(input("Ingrese su contraseña: "))
 print(usuario == adrian and contraseña == contraseña)
 
