29cff64b12587cfeb313292603afac6398e8f2ad
adrian dañon <adriancanon@unisangil.edu.co>
2025-03-12 14:33:53 -0500

    mm

---
+++ estructurascontrol.py
@@ -0,0 +1,20 @@
+ejemplo1=input(int("Ingrese un valor: "))
+if ejemplo1>5:
+    print("El valor de la variable ejemplo1 es mayor a 5")
+    print("fuera de estructura de control")
+else:
+
+    print("El valor de la variable ejemplo1 es menor o igual a 5")
+
+if ejemplo1==1:
+    print("El valor de la variable ejemplo1 es 1")
+elif ejemplo1==2:
+    print("El valor de la variable ejemplo1 es 2")
+elif ejemplo1==3:
+    print("El valor de la variable ejemplo1 es 3")
+elif ejemplo1==4:
+    print("El valor de la variable ejemplo1 es 4")
+elif ejemplo1==5:
+    print("El valor de la variable ejemplo1 es 5")
+else:
+    print("error")
