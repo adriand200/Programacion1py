40d5bbbaec9d2a5d6d1d62d604f329d1b329cfd4
adrian dañon <adriancanon@unisangil.edu.co>
2025-04-02 14:27:32 -0500

    uso del for

---
+++ for.py
@@ -0,0 +1,14 @@
+for i in range(10):#muestra un estado anterior al especidficado 
+    print(i)
+
+for i in range(5 , 10):#muestra un estado posterior al especidficado 
+    print(i)
+for i in range(0 , 10 , 2):
+    print(i)
+#variable
+x="python"
+for letras in x:
+    if letras == "h":#para q pare en t
+        break
+    else:
+        print(letras)
