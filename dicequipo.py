36786eaffa4dbc948c8b367dd5db635019856193
adrian dañon <adriancanon@unisangil.edu.co>
2025-04-23 14:23:52 -0500

    f

---
+++ dicequipo.py
@@ -0,0 +1,14 @@
+# Definición del equipo con jugadores, presidente y entrenador
+millos = {
+    "jugadores": [
+        {"nombre": "Luis", "posicion": "delantero", "dorsal": 25},
+        {"nombre": "Alberto", "posicion": "defensa", "dorsal": 30},
+        {"nombre": "Juan", "posicion": "portero", "dorsal": 28},
+        {"nombre": "Carlos", "posicion": "medio", "dorsal": 26},
+        {"nombre": "Andres", "posicion": "defensa", "dorsal": 29},
+        {"nombre": "Diego", "posicion": "delantero", "dorsal": 27},
+        {"nombre": "Santiago", "posicion": "delantero", "dorsal": 24},
+    ],
+    "presidente": "Alberto",
+    "entrenador": "Luis",
+}
